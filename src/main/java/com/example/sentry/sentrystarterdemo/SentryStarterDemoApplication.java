445a57c9241d7c5f6b2de3a84421bcac13e313cc
Austin Dase <adase@fundrise.com>
2023-11-08 13:48:03 -0500

    init

---
+++ src/main/java/com/example/sentry/sentrystarterdemo/SentryStarterDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.sentry.sentrystarterdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SentryStarterDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SentryStarterDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/sentry/sentrystarterdemo/SentryStarterDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sentry.sentrystarterdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SentryStarterDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
